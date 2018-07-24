11bb652648870d6e6de5c53e7a630cd58c394b07
denizsokullu <dsokullu@andrew.cmu.edu>
2018-07-24 12:10:13 +0100

    react-create-app on Heroku

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+// Component imports
+
+import Projects from './components/Projects';
+
 class App extends Component {
   render() {
     return (
@@ -10,6 +14,7 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
+        <Projects/>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
---
+++ src/components/Projects.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+
+class Projects extends Component {
+  render(){
+    return (
+      <h1>Here are my projects</h1>
+    )
+  }
+}
+
+export default Projects;
